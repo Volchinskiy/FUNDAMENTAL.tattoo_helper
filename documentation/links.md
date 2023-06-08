6c0384450b781ff46572098aa57aa4528137b78d
Volchinskiy Maxim <thyxaxa@gmail.com>
2023-06-08 15:28:34 -0400

    reinit repo

--- documentation/links.md
+++ documentation/links.md
@@ -1,2 +1,3 @@
-[Repo](https://gitlab.com/volchinskiy/fundamental.tattoo_helper)
+[Github Repo](https://github.com/Volchinskiy/FUNDAMENTAL.tattoo_helper)
+[Gitlab Repo](https://gitlab.com/volchinskiy/fundamental.tattoo_helper)
 [Tasks Dashboard](https://trello.com/b/av1uVNvi/fundamentaltattohelper)
--- source/application.js
+++ source/application.js
@@ -1,26 +1,26 @@
-import express from "express";
-import { webhookCallback } from "grammy";
-import { TelegramManager } from './telegram_bot_manager.class.js';
-
-if (process.env.IS_PRODUCTION === "true") {
-  const server = express();
-  server.use(express.json());
-  server.use(webhookCallback(telegramBotManager.BOT, "express"));
-
-  const PORT = process.env.PORT || 3000;
-  server.listen(PORT, () => console.log(` BOT STARTED ON ${PORT} PORT `));
-} else {
-  telegramBotManager.BOT.start();
-  console.log(` BOT STARTED `)
-}
-
 // import express from "express";
+// import { webhookCallback } from "grammy";
 // import TelegramManager from "./telegramManager/telegramManager.js";
 
-// const app = express();
-// const PORT = process.env.PORT || 8080;
+// if (process.env.IS_PRODUCTION === "true") {
+//   const server = express();
+//   server.use(express.json());
+//   server.use(webhookCallback(TelegramManager.bot, "express"));
 
-// app.listen(PORT, () => {
+//   const PORT = process.env.PORT || 3000;
+//   server.listen(PORT, () => console.log(` BOT STARTED ON ${PORT} PORT `));
+// } else {
 //   TelegramManager.botStarted();
-//   console.log("Server has been started ...");
-// });
+//   console.log(` BOT STARTED `)
+// }
+
+import express from "express";
+import TelegramManager from "./telegramManager/telegramManager.js";
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  TelegramManager.botStarted();
+  console.log("Server has been started ...");
+});
